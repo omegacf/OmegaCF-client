a555c7b5f26197100632462eeb232d1a32429668
Simple-codinger <vincentgierisch@gmail.com>
2019-08-11 01:50:39 +0200

    add create logic

--- src/factory/GameFactory.cpp
+++ src/factory/GameFactory.cpp
@@ -1,5 +1,24 @@
 #include "../../include/factory/GameFactory.hpp"
 
 Game GameFactory::create(int sizeX, int sizeY, int playerNumber){
+    std::vector<std::vector<uint8_t>> mapArray(sizeY, std::vector<uint8_t>(sizeX, 0));
+    Grid grid(mapArray);
     
+    //init players
+    std::vector<Player> players;
+    for(uint8_t i = 1; i<=playerNumber; i++){
+        Player p(i);
+        players.push_back(p);
+    }
+
+    std::vector<Field> fields;
+    //init Fields
+    for(int y = 0; y < sizeY; y++){
+        for(int x = 0; x < sizeX; x++){
+            Field field(x, y);
+            fields.push_back(field);
+        }
+    }
+
+    return Game(sizeX, sizeY, grid, players, fields);
 }
