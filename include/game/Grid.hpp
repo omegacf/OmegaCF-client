8735d146881734e5d272e503260e29d578cd4329
Simple-codinger <vincentgierisch@gmail.com>
2019-08-11 01:49:56 +0200

    remove duplicated constructor

--- include/game/Grid.hpp
+++ include/game/Grid.hpp
@@ -8,15 +8,17 @@ class Grid{
     public:
         std::vector<std::vector<uint8_t>> MapArray;
 
+        Grid(){};
+        Grid(std::vector<std::vector<uint8_t>> mapArray) : MapArray(mapArray){};
+
         inline uint8_t& getStone(int x, int y){
             return MapArray[x][y];
         }
 
-        Grid(std::vector<std::vector<uint8_t>> mapArray) : MapArray(mapArray) {};
         friend std::ostream& operator << (std::ostream &out, const Grid &g){
             for(std::vector<uint8_t> iv : g.MapArray){
                 for(uint8_t i : iv){
-                    out << c << " ";
+                    out << i << " ";
                 }
                 out << std::endl;
             }
