188a796f8b755c32785970e5f2e2d6d5dc0ce64e
Simple-codinger <vincentgierisch@gmail.com>
2019-08-11 02:01:30 +0200

    add testing code

--- src/GameHandler.cpp
+++ src/GameHandler.cpp
@@ -2,15 +2,23 @@
 
 GameHandler::GameHandler(std::string host, unsigned short port){
     Debug::printLine("Init ai");
+    /*
     if(DataHandlingService::getInstance().start(host, port) < 0){
         //cant connect to the server
         exit(0);
     }
+    */
 }
 
 void GameHandler::run(){
     Debug::printLine("Run AI");
-
+    //this is for testing purposes only!!!
+    this->_game = GameFactory::create(10, 10, 2);
+    
+    if(Debug::getFlag()){
+        std::cout << this->_game.CurrentMap;
+    }
+    
     Debug::printLine("End of the game. Bye!");
 }
 
