062f36fe038d42815498c08d37752a1b1b73727e
Simple-codinger <vincentgierisch@gmail.com>
2019-08-11 02:31:48 +0200

    add setStone logic

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -2,24 +2,40 @@
 
 std::array<int, 2> getNeighbourCoordinates(Field f, uint8_t direction){
     switch (direction)
-        {
-            case 0:
-                return {{f.X, f.Y-1}};
-            case 1:
-                return {{f.X + 1, f.Y + 1}};
-            case 2:
-                return {{f.X+1, f.Y}};
-            case 3:
-                return {{f.X+1, f.Y+1}};
-            case 4:
-                return {{f.X, f.Y+1}};
-            case 5:
-                return {{f.X-1, f.Y+1}};
-            case 6: 
-                return {{f.X-1, f.Y}};
-            case 7:
-                return {{f.X-1, f.Y-1}};
-            default:
-                return {{f.X, f.Y}};
+    {
+        case 0:
+            return {{f.X, f.Y-1}};
+        case 1:
+            return {{f.X + 1, f.Y + 1}};
+        case 2:
+            return {{f.X+1, f.Y}};
+        case 3:
+            return {{f.X+1, f.Y+1}};
+        case 4:
+            return {{f.X, f.Y+1}};
+        case 5:
+            return {{f.X-1, f.Y+1}};
+        case 6: 
+            return {{f.X-1, f.Y}};
+        case 7:
+            return {{f.X-1, f.Y-1}};
+        default:
+            return {{f.X, f.Y}};
+    }
+}
+
+void Game::setStone(Player player, int x, Grid& grid){
+    int y = 0;
+    for(int i = 0; i < this->SizeY; i++){
+        if(grid.MapArray[i][x] == 0){
+            y++;
+        }else{
+            break;
         }
+    }
+    grid.getStone(x, y-1) = player.Id;
+}
+
+std::vector<PossibleMove> Game::getPossibleMoves(Player player, Grid grid){
+
 }
