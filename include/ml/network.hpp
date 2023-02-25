2962113abcd3fb0677ed96e6bb1347a1bcd2c998
Simple-codinger <vincentgierisch@gmail.com>
2023-02-25 04:41:59 +0100

    add pytorch struct

--- include/ml/network.hpp
+++ include/ml/network.hpp
@@ -1,23 +1,78 @@
 #ifndef NETWORK_HPP
 #define NETWORK_HPP
 
-#include <tensorflow/c/c_api.h>
 #include <vector>
+#include <string>
+#include <torch/torch.h>
 
 #include "../game/Grid.hpp"
 
-using namespace tensorflow;
-using namespace tensorflow::ops;
 
 class Network {
     private:
-        Scope _model;
+        torch::nn::Module const _model;
         std::vector<int8_t> _GridToInput(Grid& grid);
+        std::string const _pathToModel = "models/";
+        std::string const _modelName;
+    
 
 
-
-    private:
+    public:
+        Network(torch::nn:Module model, std::string modelName): _model(model), _modelName(modelName) {};
         auto evalPosition(Grid& grid);
-
+        void update(); 
+        void load();
+        void save();
 };
 #endif
+
+/*
+import numpy as np
+from keras.models import Sequential, load_model
+from abc import ABC, abstractmethod
+import os
+
+save_dir = '../saved_models'
+
+
+class Network(ABC):
+    def __init__(self, model):
+        self.model = model
+        self.load()
+
+    def eval_position(self, board_state):
+        input = self._board_states_to_inputs([board_state])
+        result = self.model.predict(input)
+        return result
+
+    def update(self, board_states, rewards):
+        inputs = self._board_states_to_inputs(board_states)
+        outputs = np.array(rewards)
+        self.model.train_on_batch(inputs, outputs)
+
+    def load(self):
+        model_path = os.path.join(save_dir, self.get_save_file())
+        if os.path.isfile(model_path):
+            self.model = load_model(model_path)
+            print('Loaded model from', model_path)
+    
+    def save(self):
+        if not os.path.isdir(save_dir):
+            os.makedirs(save_dir)
+        model_path = os.path.join(save_dir, self.get_save_file())
+        self.model.save(model_path)
+
+    def _board_states_to_inputs(self, board_states):
+        inputs = np.array(board_states)
+        inputs = np.expand_dims(inputs, axis=3)
+
+        return inputs
+
+    @abstractmethod
+    def get_save_file(self):
+        pass
+
+    @abstractmethod
+    def get_name(self):
+        pass
+*/
