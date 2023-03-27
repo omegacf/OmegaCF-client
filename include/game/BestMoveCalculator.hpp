2700a06549798cd6e641a3d157c3f0de99f33fcc
Simple-codinger <vincentgierisch@gmail.com>
2023-03-27 18:21:26 +0200

    improve evaluation function

--- include/game/BestMoveCalculator.hpp
+++ include/game/BestMoveCalculator.hpp
@@ -8,6 +8,8 @@
 #include "IBestMoveCalculator.hpp"
 #include <limits>
 #include <algorithm>
+#include "../util/enums.hpp"
+
 
 class BestMoveCalculator: public IBestMoveCalculator {
     private:
@@ -17,10 +19,11 @@ class BestMoveCalculator: public IBestMoveCalculator {
         int _max = std::numeric_limits<int>::max();
         int _min = std::numeric_limits<int>::min();
         int _depth = 6;
+        int _countFreeStones(LineType type, std::pair<int, int>& begin, std::pair<int, int>& end, Grid& grid, Player& player, int numberOfStones);
     public:
         BestMoveCalculator(Game* game, Player player, int depth) : _game(game), _player(player), _depth(depth) {};
         BestMoveCalculator() {};
-        int evaluateBoard(Grid grid, Player player, Player opponent);
+        int evaluateBoard(Grid& grid, Player player, Player opponent);
 
         virtual PossibleMove getBestMove(Grid& grid);
         PossibleMove minimax(Grid& grid, int depth, int alpha = -100000, int beta = 100000, bool isMax = true);
--- src/game/BestMoveCalculator.cpp
+++ src/game/BestMoveCalculator.cpp
@@ -1,7 +1,38 @@
 #include "../../include/game/BestMoveCalculator.hpp"
 
-int BestMoveCalculator::evaluateBoard(Grid grid, Player player, Player opponent) {
+int BestMoveCalculator::evaluateBoard(Grid& grid, Player player, Player opponent) {
     int score = 0;
+
+    if (Game::checkLine(4, grid, player).size()) {
+        score += 1000;
+    }
+
+    // 3
+    // 100 for 2 empty Neighbours
+    // 40 for 1 empty neighbour
+    // 0 for 0 empty neighbours
+    std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> threeEvaluation = Game::checkLine(3, grid, player);
+    for(auto& element : threeEvaluation) {
+        int emptyNeighbours = this->_countFreeStones(std::get<0>(element), std::get<1>(element), std::get<2>(element), grid, player, 3);
+        if (emptyNeighbours == 1)
+            score += 40;
+        else if (emptyNeighbours == 2)
+            score += 100;
+    }
+
+    // 2
+    // 10 for 2 empty Neighbours
+    // 4 for 1 empty neighbour
+    // 0 for 0 empty neighbours
+    std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> twoEvaluation = Game::checkLine(2, grid, player);
+    for(auto& element : twoEvaluation) {
+        int emptyNeighbours = this->_countFreeStones(std::get<0>(element), std::get<1>(element), std::get<2>(element), grid, player, 2);
+        if (emptyNeighbours == 1)
+            score += 4;
+        else if (emptyNeighbours == 2)
+            score += 10;
+    }
+
     /*
     if (Game::checkLine(4, grid, player)) {
         score += 100;
@@ -16,6 +47,44 @@ int BestMoveCalculator::evaluateBoard(Grid grid, Player player, Player opponent)
     }
 */
     return score;
+}
+
+int BestMoveCalculator::_countFreeStones(LineType type, std::pair<int, int>& begin, std::pair<int, int>& end, Grid& grid, Player& player, int numberOfStones) {
+    int result = 0;
+    // make sure that begin is left and end is right
+    std::pair<int, int> cpyBegin = begin;
+    begin = std::min(begin.first, end.first)?begin:end;
+    end = std::max(cpyBegin.first, end.first)?end:cpyBegin;
+    switch (type)
+    {
+        case LineType::Horizontal:
+            if(begin.first > 0 && grid.getStone(begin.first-1, begin.second) != 0)
+                ++result;
+            if(end.first < grid.SizeX-1 && grid.getStone(end.first+1, end.second) != 0)
+                ++result;
+            break;
+        case LineType::Vertical:
+            if(begin.second > 0 && grid.getStone(begin.first, begin.second-1) != 0)
+                ++result;
+            if(end.second < grid.SizeY-1 && grid.getStone(end.first, end.second+1) != 0)
+                ++result;
+            break;     
+        case LineType::AscendingDiagonal:
+            if((begin.first>0 && begin.second<grid.SizeY-1) && grid.getStone(begin.first-1, begin.second+1) != 0)
+                ++result;
+            if((end.second>0 && end.first<grid.SizeX-1) && grid.getStone(end.first+1, end.second-1) != 0)
+                ++result;
+            break;    
+        case LineType::DescendingDiagonal:
+            if((begin.first>0 && begin.second>0) && grid.getStone(begin.first-1, begin.second-1) != 0)
+                ++result;
+            if((end.first<grid.SizeX-1 && end.second<grid.SizeY-1) && grid.getStone(end.first+1, end.second+1) != 0)
+                ++result;
+            break;       
+        default:
+            break;
+    }
+    return result;
 }
 
 PossibleMove BestMoveCalculator::getBestMove(Grid& grid) {
@@ -36,19 +105,19 @@ PossibleMove BestMoveCalculator::minimax(Grid& grid, int depth, int alpha, int b
             pm.Score = this->evaluateBoard(grid, this->_player, this->getOpponent(this->_player));
             return pm;
     }
-    /*
-    if (Game::checkLine(4, grid, this->_player)) {
+    
+    if (Game::checkLine(4, grid, this->_player).size()) {
         PossibleMove pm;
         pm.Score = this->_max;
         return pm;
     }
 
-    if (Game::checkLine(4, grid, this->getOpponent(this->_player))) {
+    if (Game::checkLine(4, grid, this->getOpponent(this->_player)).size()) {
         PossibleMove pm;
         pm.Score = this->_min;
         return pm;
     }
-    */
+    
 
     if (isMax) {
         std::vector<PossibleMove> possibleMoves = Game::getPossibleMoves(this->_player, grid);
