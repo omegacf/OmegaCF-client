378b8aef0443a6d9b9b7ef0a70ec49e65a739399
Simple-codinger <vincentgierisch@gmail.com>
2023-03-27 02:05:51 +0200

    fix checkLine function

--- src/game/Game.cpp
+++ src/game/Game.cpp
@@ -45,11 +45,13 @@ std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> Game
     for (int x = 0; x < grid.SizeX; x++){
         for (int y = 0; y < grid.SizeY-(numberOfStones-1) ; y++){
             int amountOfStonesInARow = 0;
-            for(int s = 0; s < (grid.SizeY - y); s++) {
-                if(grid.getStone(x, y + s) == player.Id) {
-                    amountOfStonesInARow++;
-                } else {
-                    break;
+            if(y==0 || grid.getStone(x, y-1) != player.Id) {
+                for(int s = 0; s < (grid.SizeY - y); s++) {
+                    if(grid.getStone(x, y + s) == player.Id) {
+                        amountOfStonesInARow++;
+                    } else {
+                        break;
+                    }
                 }
             }
 
@@ -69,11 +71,13 @@ std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> Game
     for (int x = 0; x < grid.SizeX-(numberOfStones-1); ++x){
         for (int y = 0; y < grid.SizeY; ++y){
             int amountOfStonesInARow = 0;
-            for(int s = 0; s < (grid.SizeX - x); ++s) {
-               if(grid.getStone(x + s, y) == player.Id) {
-                    amountOfStonesInARow++;
-                } else {
-                    break;
+            if(x==0 || grid.getStone(x-1, y) != player.Id) {
+                for(int s = 0; s < (grid.SizeX - x); ++s) {
+                if(grid.getStone(x + s, y) == player.Id) {
+                        amountOfStonesInARow++;
+                    } else {
+                        break;
+                    }
                 }
             }
 
@@ -92,13 +96,15 @@ std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> Game
     for (int x=0; x < grid.SizeX-(numberOfStones-1); ++x){
         for (int y=0; y < grid.SizeY-(numberOfStones-1); ++y){
             int amountOfStonesInARow = 0;
-            for(int s = 0; s < std::min(grid.SizeX - x, grid.SizeY - y); ++s) {
-               if(grid.getStone(x + s, y + s) == player.Id) {
-                    amountOfStonesInARow++;
-                } else {
-                    break;
-                }
-            }   
+            if((y==0 || x==0) || grid.getStone(x-1, y-1) != player.Id) {
+                for(int s = 0; s < std::min(grid.SizeX - x, grid.SizeY - y); ++s) {
+                if(grid.getStone(x + s, y + s) == player.Id) {
+                        amountOfStonesInARow++;
+                    } else {
+                        break;
+                    }
+                }   
+            }
 
             if (amountOfStonesInARow == numberOfStones) {
                 result.push_back(
@@ -115,11 +121,13 @@ std::vector<std::tuple<LineType, std::pair<int, int>, std::pair<int, int>>> Game
     for (int x=0; x < grid.SizeX-(numberOfStones-1); ++x){
         for (int y=(numberOfStones-1); y < grid.SizeY; ++y){
             int amountOfStonesInARow = 0;
-            for(int s = 0; s < std::min(grid.SizeX - x, y); ++s) {
-               if(grid.getStone(x + s, y - s) == player.Id) {
-                    amountOfStonesInARow++;
-                } else {
-                    break;
+            if((y==(grid.SizeY-1) || x==(grid.SizeX-1)) || grid.getStone(x+1, y+1) != player.Id) {
+                for(int s = 0; s < std::min(grid.SizeX - x, y); ++s) {
+                if(grid.getStone(x + s, y - s) == player.Id) {
+                        amountOfStonesInARow++;
+                    } else {
+                        break;
+                    }
                 }
             }
 
